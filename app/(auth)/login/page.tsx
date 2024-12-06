c28a1fe7d031de5866bbdf8bd8121ddb2b1b7df0
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2024-12-06 16:22:07 +0000

    (feat) add login implementation

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,20 +1,62 @@
+"use client";
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Link from 'next/link'
+import { auth } from "@/firebase"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import React, { FormEvent, useState } from "react"
+import { useRouter } from 'next/navigation'
 
 export default function Login() {
+  const [email, setEmail] = useState(""); // State for email
+  const [password, setPassword] = useState(""); // State for password
+  const [error, setError] = useState<string | null>(null); // State for error messages
+  const [success, setSuccess] = useState<string | null>(null); // State for success messages
+  const [isLoading, setIsLoading] = useState(false); // State for loading indicator
+  const router = useRouter();
+
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null); // Reset error state
+    setIsLoading(true); // Set loading state to true
+    try {
+      await signInWithEmailAndPassword(auth, email, password); // Sign in user
+      router.push("/verifyemail");
+    } catch (error: any) {
+      setError(error.message); // Set error message
+    } finally {
+      setIsLoading(false); // Reset loading state
+    }
+  };
+
+  const loadingIndicator = (
+    <div role="status" className="flex justify-center items-center h-24 text-white ">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+      <circle fill="#fff" stroke="#000000" strokeWidth="15" r="35" cx="40" cy="100">
+        <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate>
+      </circle>
+      <circle fill="#fff" stroke="#000000" strokeWidth="15" r="35" cx="100" cy="100">
+        <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate>
+      </circle>
+      <circle fill="#fff" stroke="#000000" strokeWidth="15" r="35" cx="160" cy="100">
+        <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate>
+      </circle>
+    </svg>
+  </div>
+  );
+
   return (
    <>
-     
-     <form action="" className="flex flex-col gap-4">
+     <form onSubmit={handleLogin} className="flex flex-col gap-4">
         <Input 
                 className="text-black p-2 mt-8 rounded-xl border"
                 placeholder='Email'
                 type="email"
                 name='email'
-               // value={phoneNumber}
-                //onChange={(e)=> setPhoneNumber(e.target.value)}
+                value={email} // Bind email state
+                onChange={(e) => setEmail(e.target.value)} // Update email state
                 />
 
     <div className="relative">
@@ -23,30 +65,18 @@ export default function Login() {
                 placeholder='*************'
                 type="password"
                 name='password'
-               // value={phoneNumber}
-                //onChange={(e)=> setPhoneNumber(e.target.value)}
+                value={password} // Bind password state
+                onChange={(e) => setPassword(e.target.value)} // Update password state
                 />
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye-fill absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
-  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-</svg>
+                {/* Eye icon for password visibility */}
     </div>
                  
-
 <Button 
-       // disabled={!phoneNumber || isPending || resendCountdown > 0}
-        //onClick={() => requestOpt()}
         className="rounded-xl text-white py-2 hover:scale-105 duration-100"
         >
-            {/* {
-                resendCountdown > 0 
-                ? `Resend OTP in ${resendCountdown}`
-                : isPending
-                ? "Sending OTP"
-                :"Send OTP"
-            } */}
-            Login
+            {isLoading ? loadingIndicator : `Login`}
         </Button>
+        {error && <p className="text-red-500">{error}</p>} {/* Display error message */}
         </form>
 
         <Button variant="ghost" className="mt-5 text-xs py-4 hover:scale-110 duration-100 hover:bg-transparent">
@@ -68,6 +98,5 @@ export default function Login() {
         </div>
    
    </>
-   
   )
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -8,7 +8,7 @@ import {
   
     createUserWithEmailAndPassword,
     sendEmailVerification,
-    signOut
+  
 }
 
 from "firebase/auth"
